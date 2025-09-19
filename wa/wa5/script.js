6fbbcdea9b6e356bee113c1d0d38615c4080aa97
LucaBarone015 <luba7610@colorado.edu>
2025-09-18 18:45:23 -0600

    Update script.js

--- wa/wa5/script.js
+++ wa/wa5/script.js
@@ -2,7 +2,7 @@ const navToggle = document.querySelector('.nav-toggle');
 const navMenu = document.querySelector('.nav-menu');
 const navBurger = document.querySelector('.nav-burger-menu');
 
-let toggle = true;
+let toggle = false;
 
 navToggle.addEventListener("click", () =>{
     if(toggle == false) {
