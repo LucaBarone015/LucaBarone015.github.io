01b16c028329964d704920a65048a19ba66baded
LucaBarone015 <luba7610@colorado.edu>
2025-09-25 19:37:25 -0600

    test

---
+++ wa/wa6/script.js
@@ -0,0 +1,19 @@
+//pop up images on hover
+//pop up page that shows up when clicked on 
+const navToggle = document.querySelector('.nav-toggle');
+const navMenu = document.querySelector('.nav-menu');
+const navBurger = document.querySelector('.nav-burger-menu');
+
+let toggle = false;
+
+navToggle.addEventListener("click", () =>{
+    if(toggle == false) {
+        navBurger.style.display = "block";
+        navBurger.ariaLabel = "nav bar";
+        toggle = true;
+    }
+    else {
+        navBurger.style.display = "none";
+        toggle = false;
+    }
+})
