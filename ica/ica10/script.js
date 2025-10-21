01023e8ce7b400c7e0c302fbf08fe764e1d9e620
LucaBarone015 <luba7610@colorado.edu>
2025-10-21 10:40:01 -0600

    ica10

---
+++ ica/ica10/script.js
@@ -0,0 +1,44 @@
+let triviaBtn = document.querySelector("#js-new-quote").addEventListener('click', newTrivia);
+
+let answerBtn = document.querySelector('#js-tweet').addEventListener('click', newAnswer);
+
+let current = {
+    question: "",
+    answer: "",
+}
+
+const endpoint = "https://trivia.cyberwisp.com/getrandomchristmasquestion";
+
+async function newTrivia() {
+  
+  try {
+    const response = await fetch(endpoint);
+    if (!response.ok) {
+        throw Error(response.statusText)
+    }
+    const json = await response.json();
+    console.log(json);
+    displayTrivia(json["question"]);
+    current.question = json["question"];
+    current.answer = json["answer"];
+  } catch (err) {
+    console.log(err)
+    alert('Failed to get new trivia')
+  }
+
+}
+
+function displayTrivia(question) { 
+    const questionText = document.querySelector('#js-quote-text');
+    questionText.textContent = question;
+    const answerText = document.querySelector("#js-answer-text");
+    answerText.textContent = "";
+}
+
+function newAnswer () {
+    //console.log("Success == answer!");
+    const answerText = document.querySelector("#js-answer-text");
+    answerText.textContent = current.answer;
+}
+
+newTrivia();
